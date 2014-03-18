5f246e44fab807d96a380388ef323a84a88697ae
ZachLiu <zachliugis@gmail.com>
2014-03-18 17:56:11 -0400

    Complete final touches of the tutorial.

--- rango/admin.py
+++ rango/admin.py
@@ -8,10 +8,12 @@ class PageInline(admin.TabularInline):
     model = Page
     extra = 3
 
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display=('name', 'views', 'likes')
     list_filter = ['views']
     inlines = [PageInline]
+
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(UserProfile)
